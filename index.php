76753d2ac3cf1a0bc799d7280154d25333adb00d
VladHilea <79455788+VladHilea@users.noreply.github.com>
2021-04-30 23:32:50 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,346 @@
+<!DOCTYPE html>
+<html lang="ro-RO" dir="ltr">
+
+<head>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="font-awesome/css/font-awesome.min.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<meta name="viewport" content="user-scalable=yes, initial-scale=1, maximum-scale=1, minimum-scale=1, width=device-width, height=device-height" />
+	<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+	<link rel="stylesheet" href="css/animate.min.css">
+	<link rel="stylesheet" href="css/owl.carousel.min.css">
+	<link rel="stylesheet" href="css/owl.theme.default.min.css">
+	<link rel="stylesheet" type="text/css" href="css/animate.css">
+	<link href="https://fonts.googleapis.com/css?family=Bitter" rel="stylesheet"> 
+	<link href="https://fonts.googleapis.com/css?family=Baloo+Paaji" rel="stylesheet"> 
+	<link href="https://fonts.googleapis.com/css?family=Baloo+Paaji" rel="stylesheet"> 
+	<link href="https://fonts.googleapis.com/css?family=Baloo+Bhai" rel="stylesheet"> 
+	<link href="https://fonts.googleapis.com/css?family=Merriweather" rel="stylesheet"> 
+	<link href="https://fonts.googleapis.com/css?family=Lobster" rel="stylesheet"> 
+	<link href="https://fonts.googleapis.com/css?family=Patua+One" rel="stylesheet"> 
+	<link href="https://fonts.googleapis.com/css?family=Righteous" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet"> 
+	<link rel="stylesheet" href="css/vegas.min.css">
+	<link rel="shortcut icon" href="images/favicon.ico" />
+	
+
+</head>
+<body>
+
+<!-- start menu transparent -->
+<nav class="navbar navbar-default navbar-fixed-top">
+  <div class="container">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="pull-right navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand page-scroll opacity-60" href="#top">Romania Comunista</a>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      
+      <ul class="nav navbar-nav navbar-right text-center ">
+        <li><a class="page-scroll" href="#about">Comunismul in Romania</a></li>
+        <li><a class="page-scroll" href="#program">Republica Comunista Romana</a></li>
+        <li><a class="page-scroll" href="#prieteni">Caracteristici</a></li>
+        <li><a class="page-scroll" href="#social">Revolutia din 1989</a></li>
+        
+        
+     
+    
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
+<!-- sfarsit menu transparent --> 
+
+
+<section  class=" height-100" id="top">
+	<section class="opacity-black position-relative height-100">
+		<div class="container display-table height-100" >
+			<div class="row vertical-align" >
+				
+			</div>
+		</div>
+	</section>
+</section>
+ 
+
+
+</section>
+<section id="about" class="bg-black">
+<section class="bg-white">
+
+	<div class="container">
+		<div class="row text-center pb pt">
+			<h1><b>Comunismul in Romania</b></h1>
+		</div>
+	</div>
+</section>
+		<div class="container pb pt" >
+			<div class="row  text-center pb pt center-block font-white">
+				<div class="col-sm-6 ">
+  					<img class="img-responsive wow slideInUp " src="images/logo.png" alt="...">
+
+   				</div>
+   				<div class="col-sm-6 ">
+  					<h4>România comunistă 1947-1989 este o denumire neoficială, folosită uneori cu referire la perioada comunistă din istoria României în care țara a fost cunoscută cu denumirile oficiale de Republica Populară Romînă, Republica Populară Română și respectiv, Republica Socialistă România. În această perioadă, Partidul Comunist Român (care s-a numit Partidul Muncitoresc Român între 1948-1965) a fost, de facto, partidul politic unic care a dictat prin guvern viața publică în România.
+
+După încheierea celui de-al Doilea Război Mondial, Uniunea Sovietică a făcut presiuni pentru includerea în guvernele postbelice a unor reprezentanți ai Partidul Comunist din România, recent reintrat în legalitate (partidul fusese interzis în 1924 pe motivul acceptării tezei cominterniste „a dreptului popoarelor oprimate din România imperialistă la autodeterminare până la despărțirea de stat”), în vreme ce liderii necomuniști erau eliminați în mod constant din viața politică. </h4>
+
+   				</div>
+  					
+			
+		</div>
+</section>
+<section id="program">
+	<div class="container pt-50">
+		<div class="row text-center pt pb">
+			<div >
+				<h1 class="text-center text-font "><b>Partidul Comunist Roman</b></h1>	
+			</div>
+		</div>
+	</div>
+</section>
+<section>
+<section class="bg-colo-1 ">
+
+
+	<div class="container pb pt" >
+			<div class="row  text-center pb pt center-block">
+				<div class="col-sm-6 ">
+  					<img class="img-responsive wow slideInUp " src="images/carol.jpg" alt="...">
+
+   				</div>
+   				<div class="col-sm-6 ">
+  					<h4>Partidul Comunist Român (PCR) a fost un partid politic creat în anul 1921 ca rezultat al scindării ramurii bolșevice de extremă stânga de Partidul Social Democrat Român. A funcționat în diferite perioade sub diferite denumiri oficiale: între 1921-1945 drept Partidul Comunist din România, și între 1954-1965 drept Partidul Muncitoresc Român. </h4>
+            <h4>În România interbelică, PCR a fost o mică organizație politică ilegală, subordonată Cominternului și implicit Uniunii Sovietice care a susținut ideologic revoluția comunistă. </h4>
+            <h4>Partidul comunist și-a început ascensiunea la putere în România după actul de la 23 august 1944, la care a participat în calitate de membru al Blocului Național Democrat. Apoi, conform strategiei staliniste și cu sprijinul trupelor sovietice, PCR și-a eliminat treptat adversarii. După abdicarea forțată a regelui Mihai I, în urma loviturii de stat de la 30 decembrie 1947, s-a instaurat Republica Populară Română. </h4>
+            <h2> Între 1947 și 1989, PCR a fost singurul partid politic oficial care a activat pe scena politică din România. </h2>
+
+   				</div>
+   			</div>
+  					
+			
+		</div>
+</section>
+<section class="bg-black font-white">
+	<div class="container pt-50">
+		<div class="row text-center pt pb">
+			<div class="col-sm-6" style="color: red;">
+			
+				<h2> <b class="counter">1104</b> <b ></b></h2>
+				<h3>Morți</h3>
+			</div>
+
+			<div class="col-sm-6" style="color: red;">
+			
+				<h2><b class="counter">3321 </b> <b> </b></h2>
+				<h3>Răniți</h3>
+			</div>
+			
+		</div>
+	</div>			
+</section>
+</section>
+</section>
+
+<section id="prieteni">
+	<div class="container pb pt">
+		<div class="row  text-center pb">
+			<div class="col-sm-12 pb-60"> <h1><b>Caracteristice</b></h1></div>
+				<div class="row  text-center pb pt center-block">
+				
+   				
+  					<h3><b>Economia Comunista</b></h3>
+            <h4>După cel de-al Doilea Război Mondial, și în România, asemeni celorlalte state est-europene se aplică o economie planificată, mai exact economie de comandă-control, cu planuri cincinale.[5]
+
+Se impune un ritm ridicat de creștere economică, în special industrializarea forțată, prin care anumite ramuri ale industriei (industria constructoare de mașini, siderurgică, chimică) trebuia să crească spectaculos, dar cu neglijarea producției bunurilor de consum. </h4>
+            <h3><b>Restricționarea libertății de emigrare a cetățenilor</b></h3>
+            <h4>Regimul comunist a fost restrictiv în ce privește libertatea de a ieși din țară, iar emigranții erau considerați trădători.</h4>
+            <h3><b>Emigrarea evreilor</b></h3>
+            <h4>În perioada 1959-1989, statul român a permis emigrarea a aproape 200.000 de etnici evrei, pentru care Israel a plătit sume importante în valută forte, produse agricole și alte diverse avantaje.
+
+România comunistă a obținut, în 1975, clauza națiunii celei mai favorizate, printr-o serie de concesii făcute Washingtonului, inclusiv acceptul de a permite emigrarea evreilor către Israel sau Statele Unite ale Americii.</h4>
+            <h3><b>Emigrarea germanilor</b></h3>
+            <h4>Circa 230.000 de șvabi și sași au fost vânduți de Nicolae Ceaușescu Germaniei, pentru care statul german a plătit aproximativ trei miliarde de mărci. 
+
+ Persoanele cărora li s-a aprobat stabilirea definitivă în străinătate au fost obligate să restituie, în valută, statului roman, cheltuielile efectuate pentru școlarizare, specializare și perfecționare, inclusiv bursele, în cadrul învățământului liceal, superior, postuniversitar și doctorat .</h4>
+
+
+            
+
+
+   			
+   			</div>
+			</div>
+		</div>
+	</div>
+	</section>
+
+  <section class="bg-black">
+
+  <div class="container">
+    <div class="row text-center pb pt" style="color: white;">
+      <h1><b>Revolutia din 1989</b></h1>
+    </div>
+  </div>
+</section>
+3
+<section id="social">
+	<div class="container  ">
+		<div class="row">
+		<div class="col-sm-6  text-center pb pt center-block">
+				<h4>Revoluția Română din 1989 a constat într-o serie de proteste, lupte de stradă și demonstrații desfășurate în România, între 16 și 25 decembrie 1989, care au dus la căderea dictatorului Nicolae Ceaușescu și la sfârșitul regimului comunist din România. Demonstrațiile din ce în ce mai ample au culminat cu procesul și execuția soților Ceaușescu. Înainte de revoluția română, toate celelalte state est-europene trecuseră în mod pașnic la democrație; România a fost singura țară din blocul estic care a trecut printr-o revoluție violentă și în care conducătorii comuniști au fost executați.</h4>
+        <h4>Vestea înăbușirii în sânge a demonstrațiilor din ziua precedentă a ajuns în scurt timp la toată populația Bucureștiului. Mulți dintre supraviețuitorii măcelului din zona centrală a orașului au fugit spre zonele industriale, unde au relatat muncitorilor cele întâmplate. După fuga lui Ceaușescu din clădirea Comitetului Central se instalează haosul în București, precedat de o stare de euforie generală. Mulțimile descătușate invadează Comitetul Central iar birourile oficialilor comuniști sunt vandalizate. Țintele preferate sunt portretele dictatorului și lucrările acestuia și ale soției lui, aruncate pe fereastră în semn de izbândă și dispreț. </h4>
+
+   			</div>
+
+        <div class="col-sm-6"><img src="images/revolutia.jpg"></div>
+
+      </div>
+      <div class="text-center"><h2>La puțin timp după aceea, în jurul orei 12:50, Televiziunea Română își reia emisia. Mircea Dinescu și Ion Caramitru apar în fruntea unui grup de revoluționari, anunțând exaltați fuga dictatorului.</h2></div>
+	</div>
+</section>
+
+
+<section class="bg-black">
+	<div class="container">
+		<div class="row text-center pb pt">
+			<div class="col-sm-12">
+				<h4>Created by Vlad Hilea</h4>
+				<h4>Copyright @ All rights reserved to Vlad Hilea</h4>
+				<h5>Vegas Slider by Jay Salvat</h5>
+			</div>
+		</div>
+	</div>
+</section>
+
+<a href="#top" id="goto" class='page-scroll dn  fs-18'>
+<span class="fa-stack fa-lg hidden-xs" style='position:fixed; bottom:60px; right:60px;'>
+  <i class="fa fa-circle fa-stack-2x"></i>
+  <i class="fa bg-yellow-1 img-circle fa-arrow-up font-white fa-stack-1x fa-inverse"></i>
+</span>
+</a>
+
+<script src="js/jquery.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="http://cdnjs.cloudflare.com/ajax/libs/waypoints/2.0.3/waypoints.min.js"></script>
+<script src="js/counterup.min.js"></script>
+ <script src="js/wow.min.js"></script>
+              <script>
+              new WOW().init();
+              </script>
+<script src="js/owl.carousel.js"></script>
+<script src="js/typed.min.js"></script>
+<script src="js/vegas.min.js"></script>
+<script>  
+jQuery(window).scroll(function() {
+    if (jQuery(".navbar").offset().top > 50) {
+        jQuery(".navbar-fixed-top").addClass("nav-collapsed");
+
+        } 
+    else
+        {
+        jQuery(".navbar-fixed-top").removeClass("nav-collapsed");
+
+        }
+});
+</script>
+<script>
+var width = $(window).width();
+if (width > 768) {jQuery(window).scroll(function() {
+    if (jQuery(".navbar").offset().top > 50) {
+        jQuery(".navbar-fixed-top").addClass("nav-collapsed");
+
+        } 
+    else
+        {
+        jQuery(".navbar-fixed-top").removeClass("nav-collapsed");
+
+        }
+});
+}
+ else{
+ 	 jQuery(".navbar-fixed-top").addClass("nav-collapsed");
+ }
+
+
+ 
+    $('.navbar-collapse ul li a:not(.dropdown-toggle)').click(function() {
+    $('.navbar-toggle:visible').click();
+});
+
+     jQuery('.page-scroll').bind('click', function(e) {
+            e.preventDefault();
+            jQuery('html,body').animate({scrollTop: jQuery(this.hash).offset().top - 50});                                                         
+               });
+       $(window).scroll(function() {    
+    var scroll = $(window).scrollTop();
+    if (scroll >= 1250) {  
+       
+        $("#goto").removeClass("dn");
+    }
+    else{
+ $("#goto").addClass("dn");
+    }
+}); 
+</script>
+<script>
+    jQuery(document).ready(function( $ ) {
+        $('.counter').counterUp({
+            delay: 10,
+            time: 1000
+        });
+    });
+</script>
+<script >
+	$(document).ready(function(){
+  $(".owl-carousel").owlCarousel();
+});
+</script>
+<script >
+	$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    center: true;
+    responsive:{
+        0:{
+            items:
+        }
+    }
+});
+</script>
+<script>
+	var typed = new Typed("#typed", {
+  strings: ["#RESPEEEECT ", "#PUTEREEEE ", "#MUIELACAI ", "#COLOSQUAD "],
+  typeSpeed: 50,
+  smartBackspace:true,
+    loop: true,
+  loopCount: Infinity,
+   backSpeed: 50,
+    backDelay: 800,
+
+});
+</script>
+<script>
+    $("#top").vegas({
+    slides: [
+        { src: "images/bg.png" },
+        { src: "images/comunism.jpg" },
+        { src: "images/stadion.jpg" }
+    ],
+    animation: 'kenburns',
+    delay:8000
+});
+</script>
+</body>
+</html>
+
+
